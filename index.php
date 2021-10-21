043ed18fbc6e3052159989e1a6fb90fd2bec783f
HimNahyeon <HimNahyeon@gmail.com>
2021-10-22 00:52:26 +0900

    common.css 파일 생성
    20211022  00:52
    common.css파일 작성

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ko">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./style/common.css">
     <title>마켓컬리 :: 내일의 장보기, 마켓컬리</title>
 </head>
 <body>
