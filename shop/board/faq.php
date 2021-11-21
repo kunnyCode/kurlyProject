f67b90474dd43c65ed2576f690b6140f9dce9819
namichin0904 <namichin0904@gmail.com>
2021-11-21 16:10:40 +0900

    Update faq.php

--- shop/board/faq.php
+++ shop/board/faq.php
@@ -756,7 +756,7 @@
                                     <li>
                                         <a href="./mypage_qna.php" onclick="KurlyTrackerLink('/shop/mypage/mypage_qna.php', 'select_service_personal_inquiry_history')">1:1문의</a>
                                     </li>
-                                    <li class="on">
+                                    <li>
                                         <a href="../main/bulk_order.php" onclick="KurlyTrackerLink('/shop/main/html.php?htmid=mypage/bulk_order.htm', 'select_service_bulk_order')">대량주문 문의</a>
                                     </li>
                                     <li>
