983175127f358680c0c0cdc90fbc37f1d1168531
DakyungLe <ell2y2@naver.com>
2021-11-30 21:09:51 +0900

    Update login.php

--- shop/service/login/login.php
+++ shop/service/login/login.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=f, initial-scale=1.0">
+    <link rel="stylesheet" href="../../CSS/common.css">
+    <link rel="stylesheet" href="../../CSS/Service_center.css">
     <link rel="stylesheet" href="../../CSS/login.css">
     <title>로그인</title>
     <style>
