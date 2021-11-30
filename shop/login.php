aea76f659a8e07297c9b063d9a3745fe082e545f
DakyungLe <ell2y2@naver.com>
2021-11-30 19:59:07 +0900

    login/login_find CSS

--- shop/login.php
+++ shop/login.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=f, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/login.css">
     <title>로그인</title>
     <style>
         *{margin: 0; padding: 0; box-sizing: border-box;}
@@ -19,40 +20,47 @@
             background-size: auto;
         }
     </style>
-    <link rel="stylesheet" href="./login.css">
 </head>
 <body>
     <div class="container">
         <section>
             <div class="login">
-                <div class="login_form"></div>
-                <h1>로그인</h1>
-                <td>
-                    <p><input type="text" name="login_id" id="login_id" maxlength="20" size="20" tabindex="1" placeholder="아이디를 입력해주세요."></p>
-                    <p><input type="text" name="login_pw" id="login_pw" maxlength="20" size="20" tabindex="2" placeholder="비밀번호를 입력해주세요."></p>
-                    </td>
+                <div class="login_form">
+                <h1 class="tit_login">로그인</h1>
+                <!-- <td> -->
+                    <div class="class1">
+                    <p><input type="text" name="login_id" id="login_id" maxlength="20" size="20" tabindex="1" placeholder="아이디를 입력해주세요."></p></div>
+
+                    <div class="class2">
+                    <p><input type="password" name="login_pw" id="login_pw" maxlength="20" size="20" tabindex="2" placeholder="비밀번호를 입력해주세요."></p></div>
+                    <!-- </td> -->
+                </div>
                     <div class="checkbox_double">
-                        <input type="checkbox"> 보안접속
-                    </div>
-                    <div class="search_idpw">
-                        <a href="#" class="link"onclick="#">아이디 찾기</a>
+                        <label class="lable_checkbox">
+                            <input type="checkbox" id="chk_security" name="chk_security" value="y" checked="checked"> 보안접속
+                        </label>
+
+                        <!-- <div class="search_idpw"> -->
+                        <a href="#" class="link"onclick="#" id="search_id">아이디 찾기</a>
+
                         <span class="ico">|</span>
-                        <a href="#" class="link" onclick="#">비밀번호 찾기</a>
-                    </div>
+
+                        <a href="#" class="link" id="search_pw">비밀번호 찾기</a>
+                    <!-- </div> -->
+                </div>
                     <div class="submit_form">
                         <td>
                             <button type="submit" class="btn_num1">
                                 <span class="text">로그인</span>
                             </button>
                         <p>
-                            <button>
-                                <a href="#" class="link" class="btn_num2"></a>
+                            <button type="submit"
+                                class="btn_num2"></a>
                                 <span class="text">회원가입</span>
                             </button>
                         </p>
                         </td>
                     </div>
-
             </div>
         </section>
     </div>
